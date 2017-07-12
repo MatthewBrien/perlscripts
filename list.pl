603aff3b4bcc948e1f30cdfe7611709c20600d98
Matthew Brien <MatthewBrien@users.noreply.github.com>
2017-07-12 15:21:03 -0400

    Update list.pl

--- list.pl
+++ list.pl
@@ -2,38 +2,46 @@
 use strict;
 use warnings;
 
+#print and append new line
 sub say { print @_, "\n" }
 sub list;
 
+#print all files in directory, call list on all subdirectories
 sub list{
-  say "Called again!";
   my $current_directory = $_[0];
-  say "opening $current_directory";
   opendir my $dir, $current_directory or die "Cannot open directory: $!";
     my @files = readdir $dir;
-  closedir $dir;
-  say @files;
+closedir $dir;
   for my $file (@files)
   {
-    if( !($file eq '.') && !($file eq '..') ){
-      if(-d $file){
-          list("$current_directory$file/");
-      }
-      if(-f $file){
-          say $current_directory . $file;
+      if(-d "$current_directory$file"){
+
+        if( !($file eq '.') && !($file eq '..') ){
+            list("$current_directory$file/");
+          }
         }
-      }
+      else{
+          say "$current_directory$file";
+        }
     }
-  }
+
+} #end list
 
 
-my $top_dir = "";
-#Pass a directory to traverse, if no parameter, default is the current directory
+#setup
+#Pass a directory to traverse, default is set to he current directory ./
+#If directory  doesn't end with /  append one
+my $top_dir;
 if (@ARGV){
   $top_dir = $ARGV[0];
+    #add slash to directory
+  if(!($top_dir =~ /\/$/ )){
+    $top_dir .= '/';
+  }
 }
 else {
   $top_dir = "./";
-}
+  }
 
+#list files in folder
 list($top_dir);
