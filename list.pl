88b5e22faafe626ba4b07555594ef07a37cb5e2a
Matthew Brien <MatthewBrien@users.noreply.github.com>
2017-08-02 16:02:19 -0400

    Update list.pl

--- list.pl
+++ list.pl
@@ -1,47 +1,103 @@
-#!/usr/local/bin/perl
-use strict;
-use warnings;
+#!/usr/bin/perl
+#generates lists of files, directories and paths
 
-#print and append new line
-sub say { print @_, "\n" }
-sub list;
-
-#print all files in directory, call list on all subdirectories
-sub list{
+#Given a directory, returns a list of absolute paths to every file
+sub list_files{
+  my @list;
   my $current_directory = $_[0];
   opendir my $dir, $current_directory or die "Cannot open directory: $!";
-    my @files = readdir $dir;
-closedir $dir;
+  my @files = readdir $dir;
+  closedir $dir;
+
   for my $file (@files)
   {
       if(-d "$current_directory$file"){
-
         if( !($file eq '.') && !($file eq '..') ){
-            list("$current_directory$file/");
+            push( @list, list_files("$current_directory$file/"));
           }
         }
       else{
-          say "$current_directory$file";
-        }
-    }
-
-} #end list
-
-
-#setup
-#Pass a directory to traverse, default is set to he current directory ./
-#If directory  doesn't end with /  append one
-my $top_dir;
-if (@ARGV){
-  $top_dir = $ARGV[0];
-    #add slash to directory
-  if(!($top_dir =~ /\/$/ )){
-    $top_dir .= '/';
+          push(@list, "$current_directory$file");
+      }
   }
+return @list;
 }
-else {
-  $top_dir = "./";
+
+#given a directory, returns a path to every sub directory
+sub list_directories{
+  my @list;
+  my $current_directory = $_[0];
+  opendir my $dir, $current_directory or die "Cannot open directory: $!";
+  my @files = readdir $dir;
+  closedir $dir;
+
+  for my $file (@files)
+  {
+      if(-d "$current_directory$file"){
+        if( !($file eq '.') && !($file eq '..') ){
+            push(@list, "$current_directory$file");
+            push( @list, list_directories("$current_directory$file/"));
+          }
+        }
+  }
+return @list;
+}
+
+sub list{
+  my ($directory_to_list, $list_type) = @_;
+  my @list;
+
+  #replace ~ with absolute path
+  if($directory_to_list =~ /^~(?<dir>.*)/){
+  $directory_to_list = "/home/".getpwuid($<)."/$+{dir}";
+  }
+  #add trailing '/'
+  if(!($directory_to_list =~/.*\/$/)){
+    $directory_to_list .= '/';
   }
 
-#list files in folder
-list($top_dir);
+  if($list_type eq "files"){
+    @list = list_files($directory_to_list);
+
+    for (my $i = 0; $i < @list; $i++){
+      $list[$i] =~ /(?<file_name>[a-zA-Z0-9\.\-\_\~]+$)/;
+      $list[$i] = $+{file_name};
+    }
+  }
+  elsif($list_type eq "directories"){
+    @list = list_directories($directory_to_list);
+    for (my $i = 0; $i < @list; $i++){
+      $list[$i] =~ /(?<dir_name>[a-zA-Z0-9\.\-\_\~]+$)/;
+      $list[$i] = $+{dir_name};
+    }
+  }
+  elsif($list_type eq "files_absolute"){
+    @list = list_files($directory_to_list);
+  }
+  elsif($list_type eq "files_relative"){
+    @list = list_files($directory_to_list);
+
+    for (my $i = 0; $i < @list; $i++){
+      my $re = qr/$directory_to_list/;
+      $list[$i] =~ /${re}(?<file_name>.*$)/;
+      $list[$i] = $+{file_name};
+    }
+  }
+  elsif($list_type eq "directories_absolute"){
+    @list = list_directories($directory_to_list);
+  }
+  elsif($list_type eq "directories_relative"){
+    @list = list_directories($directory_to_list);
+    for (my $i = 0; $i < @list; $i++){
+      my $re = qr/$directory_to_list/;
+      $list[$i] =~ /${re}(?<file_name>.*$)/;
+      $list[$i] = $+{file_name};
+    }
+  }
+  else{
+      print "list takes a directory path, and one of the following parameters: 'files, files_relatve, files_absolute, directories, directories_relative, or directories_absolute\n";
+  }
+
+  return @list;
+}
+1;
